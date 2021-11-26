71dca512729a07b623697a9a63e853847f97bdab
исп-31 <-31@LAB17-13>
2021-11-26 14:53:12 +0300

    Добавьте файлы проекта.

---
+++ Lib_14/Practice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_14
+{
+    public class Practice
+    {
+        public static int Sum(int[] mas)
+        {
+            int sum = mas[0];
+            for (int i = 1; i < mas.Length; i++)
+            {
+                if (mas[i] < 8)
+                    sum += mas[i];
+                else sum += 0;
+                
+            }
+            return sum;
+        }
+    }
+}
